b066df6011a4b53e40cb5b1c2806d507f3f52721
Enrique Salmerón <jesalmeronleal@gmail.com>
2024-01-17 21:16:01 -0600

    MenuService was added

---
+++ technicalAssessment/src/main/java/com/mycompany/technicalassessment/MenuService.java
@@ -0,0 +1,73 @@
+package com.mycompany.technicalassessment;
+
+import java.util.Scanner;
+
+public class MenuService {
+    private final ValidationService validationService;
+    private final IntegerToRomanService toRoman;
+    private final RomanToIntegerService toNumber;
+
+    public MenuService(ValidationService validationService, IntegerToRomanService toRoman, RomanToIntegerService toNumber) {
+        this.validationService = validationService;
+        this.toRoman = toRoman;
+        this.toNumber = toNumber;
+    }
+
+    public void startMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Hi! Welcome to my Technical Assessment");
+
+            int option;
+            do {
+                displayMenuOptions();
+                option = scanner.nextInt();
+                scanner.nextLine();
+
+                processOption(option, scanner);
+            } while (option != 3);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error in validation: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
+    private void displayMenuOptions() {
+        System.out.println("Please select an option: | 1.Integer to Roman | 2.Roman to Integer | 3.Exit");
+    }
+
+    private void processOption(int option, Scanner scanner) {
+        switch (option) {
+            case 1:
+                processIntegerToRoman(scanner);
+                break;
+            case 2:
+                processRomanToInteger(scanner);
+                break;
+            case 3:
+                System.out.println("Bye!");
+                break;
+            default:
+                System.out.println("Invalid option. Please select a valid option.");
+        }
+    }
+
+    private void processIntegerToRoman(Scanner scanner) {
+        System.out.println("Please provide an integer: ");
+        int integer = scanner.nextInt();
+        scanner.nextLine(); // Consume newline left by nextInt()
+        validationService.validateNumber(integer);
+        String roman = toRoman.converterRoman(integer);
+        System.out.println("The roman of " + integer + " is: " + roman);
+    }
+
+    private void processRomanToInteger(Scanner scanner) {
+        System.out.println("Please provide a roman: ");
+        String romanNum = scanner.nextLine();
+        validationService.validateRoman(romanNum);
+        int number = toNumber.convertToInteger(romanNum);
+        System.out.println("The integer of " + romanNum + " is: " + number);
+    }
+}
--- technicalAssessment/src/main/java/com/mycompany/technicalassessment/TechnicalAssessment.java
+++ technicalAssessment/src/main/java/com/mycompany/technicalassessment/TechnicalAssessment.java
@@ -12,17 +12,11 @@ import java.util.Scanner;
 public class TechnicalAssessment {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         ValidationService validationService = new ValidationService();
         IntegerToRomanService toRoman = new IntegerToRomanService(validationService);
-        try {
-            System.out.println("Hi! Welcome to my Technical Assessment");
-            System.out.println("Please provide an integer: ");
-            int number = scanner.nextInt();
-            String roman = toRoman.converterRoman(number);
-            System.out.println("The roman of " + number + " is: " + roman);
-        }catch(IllegalArgumentException e) {
-            System.err.println("Error in validation: " + e.getMessage());
-        }
+        RomanToIntegerService toNumber = new RomanToIntegerService(validationService);
+        MenuService menuService = new MenuService(validationService, toRoman, toNumber);
+        menuService.startMenu();
     }
+
 }
