264a71f2dcbca2671ff8f882d32a4411030a56e2
Enrique Salmerón <jesalmeronleal@gmail.com>
2024-01-17 18:40:03 -0600

    First commit
    Project was created

---
+++ technicalAssessment/src/main/java/com/mycompany/technicalassessment/TechnicalAssessment.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.technicalassessment;
+
+/**
+ *
+ * @author José Enrique Salmerón Leal
+ */
+
+public class TechnicalAssessment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
